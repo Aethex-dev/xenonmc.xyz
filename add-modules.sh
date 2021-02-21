69dfe9c588917ad5e41638cf87f56138add98ceb
Lucas Burlingham <41302403+lucasburlingham@users.noreply.github.com>
2021-02-21 18:38:20 -0500

    nm

--- add-modules.sh
+++ add-modules.sh
@@ -3,7 +3,6 @@
 Yellow='\033[1;33m'
 GREEN='\033[0;32m'
 NC='\033[0m'                                                            # No Color
-echo -e "${YELLOW}"
 git submodule add https://github.com/XENONMC-DEV/XFRAME >/dev/null 2>&1 # redirect any errors to /dev/null
 git clone https://github.com/XENONMC-DEV/XFRAME >/dev/null 2>&1         # clone just incase there is an error
 git submodule add https://github.com/XENONMC-DEV/XFRAME-ROUTER >/dev/null 2>&1
