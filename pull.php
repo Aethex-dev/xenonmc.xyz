35e1cf1da33f3078a988ae4c9eaeb5a24f5d8cd8
Lucas Burlingham <lucas.burlingham@icloud.com>
2021-03-01 23:12:37 -0500

    test1

--- pull.php
+++ pull.php
@@ -1,5 +1,7 @@
 <?php
-$output = shell_exec("./add-modules.sh");
+// $output = shell_exec("./add-modules.sh");
+$output = shell_exec('ls | grep -Ev \'.html|.md|.less|.sh|.jpg|.png|.webp|.xml|.txt|.js|.php|.jpeg\' | while read -r output; do cd $output && git pull | grep -v "Not a directory" && cd ..; done
+');
 
 ?>
 <!DOCTYPE html>
