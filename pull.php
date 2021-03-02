2717bae2b654749c68f33e4eac1de153a13572c3
Lucas Burlingham <lucas.burlingham@icloud.com>
2021-03-01 23:15:25 -0500

    Added style.less linkage

--- pull.php
+++ pull.php
@@ -10,12 +10,12 @@ $output = shell_exec('ls | grep -Ev \'.html|.md|.less|.sh|.jpg|.png|.webp|.xml|.
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="/logo.png" type="image/x-icon">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="Description" content="Enter your description here" />
+    <link rel="shortcut icon" href="/logo.webp" type="image/x-icon">
+    <!-- load sheet then javascript -->
+    <link rel="stylesheet/less" type="text/css" href="styles.less" />
+    <script src="//cdn.jsdelivr.net/npm/less@3.13"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
-    <link rel="stylesheet" href="assets/css/style.css">
     <title>XenonMC Development: Github Pull</title>
 </head>
 
@@ -35,7 +35,7 @@ BASH:
         </pre>
     </div>
     <hr>
-    <button class="btn btn-success centered" href="/index.html">Back to Home</button>
+    <a class="btn btn-success centered" href="/index.html">Back to Home</a>
     <script src="/upup.js"></script>
     <script>
         UpUp.start({
