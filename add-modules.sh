8a2e5b8278a49bb24d9a9d3ca32d54a4126365da
Lucas Burlingham <lucas.burlingham@icloud.com>
2021-03-01 23:01:58 -0500

    !!

--- add-modules.sh
+++ add-modules.sh
@@ -3,14 +3,14 @@ echo "$output"
 output=$(git submodule add https://github.com/XENONMC-DEV/XFRAME)
 echo "$output"
 output=$(git submodule add https://github.com/XENONMC-DEV/XFRAME)
-git clone https://github.com/XENONMC-DEV/XFRAME >/dev/null 2>&1
-git submodule add https://github.com/XENONMC-DEV/XFRAME-ROUTER >/dev/null 2>&1
-git clone https://github.com/XENONMC-DEV/XFRAME-ROUTER >/dev/null 2>&1
-git submodule add https://github.com/XENONMC-DEV/tools >/dev/null 2>&1
-git clone https://github.com/XENONMC-DEV/tools >/dev/null 2>&1
-git submodule add https://github.com/XENONMC-DEV/PHP-MySQLi-Query-Builder >/dev/null 2>&1
-git clone https://github.com/XENONMC-DEV/PHP-MySQLi-Query-Builder >/dev/null 2>&1
-git submodule add https://github.com/XENONMC-DEV/XFORUM >/dev/null 2>&1
-git clone https://github.com/XENONMC-DEV/XFORUM >/dev/null 2>&1
+git clone https://github.com/XENONMC-DEV/XFRAME
+git submodule add https://github.com/XENONMC-DEV/XFRAME-ROUTER
+git clone https://github.com/XENONMC-DEV/XFRAME-ROUTER
+git submodule add https://github.com/XENONMC-DEV/tools
+git clone https://github.com/XENONMC-DEV/tools
+git submodule add https://github.com/XENONMC-DEV/PHP-MySQLi-Query-Builder
+git clone https://github.com/XENONMC-DEV/PHP-MySQLi-Query-Builder
+git submodule add https://github.com/XENONMC-DEV/XFORUM
+git clone https://github.com/XENONMC-DEV/XFORUM
 ls | while read -r output; do cd $output && git pull | grep -v "Not a directory" && cd ..; done
 echo "Completed!"
