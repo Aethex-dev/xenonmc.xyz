cecbefd66c3b4fbde5b8505fb3d6b15a7a39d25a
Lucas Burlingham <lucas.burlingham@icloud.com>
2021-03-01 22:50:51 -0500

    remove colors for better compatibility

--- add-modules.sh
+++ add-modules.sh
@@ -1,9 +1,5 @@
 # E Lucas Burlingham Mon Feb 22 07:57:55 PM EST 2021
 #!/bin/bash
-# Set colors
-Yellow='\033[1;33m'
-GREEN='\033[0;32m'
-NC='\033[0m'                                                            # No Color
 git pull                                                                # Pull the latest version of the script for the next time the user runs it
 git submodule add https://github.com/XENONMC-DEV/XFRAME >/dev/null 2>&1 # redirect any errors to /dev/null
 git clone https://github.com/XENONMC-DEV/XFRAME >/dev/null 2>&1         # clone just incase there is an error
@@ -15,5 +11,5 @@ git submodule add https://github.com/XENONMC-DEV/PHP-MySQLi-Query-Builder >/dev/
 git clone https://github.com/XENONMC-DEV/PHP-MySQLi-Query-Builder >/dev/null 2>&1 # clone just incase there is an error
 git submodule add https://github.com/XENONMC-DEV/XFORUM >/dev/null 2>&1
 git clone https://github.com/XENONMC-DEV/XFORUM >/dev/null 2>&1                                 # clone just incase there is an error
-ls | while read -r output; do cd $output | grep -v "Not a directory" && git pull && cd ..; done # for each directory in the current directory, change into that directory and run `git pull`
-echo -e "${GREEN}Completed!${NC}"
+ls | while read -r output; do cd $output && git pull | grep -v "Not a directory" && cd ..; done # for each directory in the current directory, change into that directory and run `git pull`
+echo -e "Completed!"
