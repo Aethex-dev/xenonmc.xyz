73eb3033a2f53d590d613d43f8aca9bfa26efde6
Lucas Burlingham <lucas.burlingham@icloud.com>
2021-03-01 23:11:29 -0500

    reverse changes from test2

--- add-modules.sh
+++ add-modules.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 git pull
-# ls | grep -Ev '.html|.md|.less|.sh|.jpg|.png|.webp|.xml|.txt|.js|.php|.jpeg' | while read -r output; do cd $output && git pull | grep -v "Not a directory" && cd ..; done
-ls | grep -v . | while read -r output; do cd $output && git pull | grep -v "Not a directory" && cd ..; done
-
+ls | grep -Ev '.html|.md|.less|.sh|.jpg|.png|.webp|.xml|.txt|.js|.php|.jpeg' | while read -r output; do cd $output && git pull | grep -v "Not a directory" && cd ..; done
 echo "Completed!"
