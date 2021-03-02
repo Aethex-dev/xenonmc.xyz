02cf88461ea70247dc0e30c5ac465569a2e0ce61
Lucas Burlingham <lucas.burlingham@icloud.com>
2021-03-01 22:59:05 -0500

    fix 2

--- add-modules.sh
+++ add-modules.sh
@@ -1,18 +1,16 @@
-# E Lucas Burlingham Mon Feb 22 07:57:55 PM EST 2021
-#!/bin/bash
-output = $('git pull')
+output = $(git pull)
 echo $output
-output = $('git submodule add https://github.com/XENONMC-DEV/XFRAME >/dev/null 2>&1')
+output = $(git submodule add https://github.com/XENONMC-DEV/XFRAME >/dev/null 2>&1)
 echo $output
-output = $('git submodule add https://github.com/XENONMC-DEV/XFRAME >/dev/null 2>&1')
-git clone https://github.com/XENONMC-DEV/XFRAME >/dev/null 2>&1 # clone just incase there is an error
+output = $(git submodule add https://github.com/XENONMC-DEV/XFRAME >/dev/null 2>&1)
+git clone https://github.com/XENONMC-DEV/XFRAME >/dev/null 2>&1
 git submodule add https://github.com/XENONMC-DEV/XFRAME-ROUTER >/dev/null 2>&1
-git clone https://github.com/XENONMC-DEV/XFRAME-ROUTER >/dev/null 2>&1 # clone just incase there is an error
+git clone https://github.com/XENONMC-DEV/XFRAME-ROUTER >/dev/null 2>&1
 git submodule add https://github.com/XENONMC-DEV/tools >/dev/null 2>&1
-git clone https://github.com/XENONMC-DEV/tools >/dev/null 2>&1 # clone just incase there is an error
+git clone https://github.com/XENONMC-DEV/tools >/dev/null 2>&1
 git submodule add https://github.com/XENONMC-DEV/PHP-MySQLi-Query-Builder >/dev/null 2>&1
-git clone https://github.com/XENONMC-DEV/PHP-MySQLi-Query-Builder >/dev/null 2>&1 # clone just incase there is an error
+git clone https://github.com/XENONMC-DEV/PHP-MySQLi-Query-Builder >/dev/null 2>&1
 git submodule add https://github.com/XENONMC-DEV/XFORUM >/dev/null 2>&1
-git clone https://github.com/XENONMC-DEV/XFORUM >/dev/null 2>&1                                 # clone just incase there is an error
-ls | while read -r output; do cd $output && git pull | grep -v "Not a directory" && cd ..; done # for each directory in the current directory, change into that directory and run `git pull`
+git clone https://github.com/XENONMC-DEV/XFORUM >/dev/null 2>&1
+ls | while read -r output; do cd $output && git pull | grep -v "Not a directory" && cd ..; done
 echo "Completed!"
