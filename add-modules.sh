cc0d53742409ac213057701fdc66ca579c853e55
Lucas Burlingham <lucas.burlingham@icloud.com>
2021-03-01 23:02:46 -0500

    Remove adding everything every time

--- add-modules.sh
+++ add-modules.sh
@@ -1,16 +1,3 @@
-output=$(git pull)
-echo "$output"
-output=$(git submodule add https://github.com/XENONMC-DEV/XFRAME)
-echo "$output"
-output=$(git submodule add https://github.com/XENONMC-DEV/XFRAME)
-git clone https://github.com/XENONMC-DEV/XFRAME
-git submodule add https://github.com/XENONMC-DEV/XFRAME-ROUTER
-git clone https://github.com/XENONMC-DEV/XFRAME-ROUTER
-git submodule add https://github.com/XENONMC-DEV/tools
-git clone https://github.com/XENONMC-DEV/tools
-git submodule add https://github.com/XENONMC-DEV/PHP-MySQLi-Query-Builder
-git clone https://github.com/XENONMC-DEV/PHP-MySQLi-Query-Builder
-git submodule add https://github.com/XENONMC-DEV/XFORUM
-git clone https://github.com/XENONMC-DEV/XFORUM
+git pull
 ls | while read -r output; do cd $output && git pull | grep -v "Not a directory" && cd ..; done
 echo "Completed!"
