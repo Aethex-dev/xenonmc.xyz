6eaaaa4d1aaa9203a04970eedf77c55027e24edc
Lucas Burlingham <lucas.burlingham@icloud.com>
2021-03-01 23:09:43 -0500

    remove asterik from `fix 5` and do it the long way

--- add-modules.sh
+++ add-modules.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git pull
-ls | grep -Ev '.html|.md|.less|.sh|.jpg|.png|.webp|.*' | while read -r output; do cd $output && git pull | grep -v "Not a directory" && cd ..; done
+ls | grep -Ev '.html|.md|.less|.sh|.jpg|.png|.webp|.xml|.txt|.js' | while read -r output; do cd $output && git pull | grep -v "Not a directory" && cd ..; done
 echo "Completed!"
