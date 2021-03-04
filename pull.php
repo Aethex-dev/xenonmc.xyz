4dcb79afde51f8b7266101080e3aa173add413cc
Lucas Burlingham <lucas.burlingham@icloud.com>
2021-03-03 22:10:44 -0500

    Remove unnessary linkings

--- pull.php
+++ pull.php
@@ -6,14 +6,9 @@ $output = shell_exec("./add-modules.sh");
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="/logo.webp" type="image/x-icon">
-    <!-- load sheet then javascript -->
-    <link rel="stylesheet/less" type="text/css" href="styles.less" />
+    <link rel="stylesheet/less" type="text/css" href="/assets/css/styles.less" />
     <script src="//cdn.jsdelivr.net/npm/less@3.13"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
-    <title>XenonMC Development: GIt Pull</title>
 </head>
 
 <body class="bg-black">
@@ -26,24 +21,6 @@ $output = shell_exec("./add-modules.sh");
             }
             ?>
         </pre>
-    <script src="/upup.js"></script>
-    <script>
-        UpUp.start({
-            'content-url': 'index.html',
-            'assets': ['https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css',
-                'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css',
-                '/styles.less', '/logo.webp',
-                'https://cdn.jsdelivr.net/npm/less@3.13',
-                'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js',
-                'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.slim.min.js',
-                'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js'
-            ],
-            'scope': '/'
-        });
-    </script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.slim.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js"></script>
 </body>
 
 </html>
